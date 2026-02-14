1274ff1d8ec028b8fa125a316eddcc78e6006150
Neehar Gadam <neehargadam@gmail.com>
2026-02-14 11:49:04 +0530

    handling default form submission using  e.preventDefault();

--- src/components/event-binding/bubbling-EventExample.jsx
+++ src/components/event-binding/bubbling-EventExample.jsx
@@ -9,13 +9,24 @@ function BubblingEventExample(){
         alert("Search Button Clicked - shows search");
     }
 
+    function SubmitClick(e){
+        e.preventDefault(); // prevents the default form submission behavior
+        alert("Form Submitted - Login");
+    }
+
     return(
         <div className="container-fluid">
             <nav onClick={NavClick} className=" p-2 border border-2 border-dark m-4">
             <h2>Amazon</h2>
-            <button onClick={SearchClick} className="btn bi bi-search btn-warning"></button>
-            
+            <button onClick={SearchClick} className="btn bi bi-search btn-warning"></button>            
             </nav>
+            <section className="m-4">
+                <form onSubmit={SubmitClick} className="border border-2 border-dark p-4">
+                    <h3>User Name</h3>
+                    <input type="text" name="User"/>
+                    <button type="submit" >Login</button>
+                </form>
+            </section>
         </div>
     )
 }
