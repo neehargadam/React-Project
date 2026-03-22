fdf252b6eaae85a616aecf5b91cf38a0e9fdeb40
Neehar Gadam <neehargadam@gmail.com>
2026-03-22 14:18:50 +0530

    disable right click in browser

--- src/components/button-demo/button-demo.jsx
+++ src/components/button-demo/button-demo.jsx
@@ -5,10 +5,18 @@ function ButtonDemo()
     {
         window.open("m1.jpg","Mobile Image","width=400,height=400");
     }
+    function handleContextMenu()
+    {
+        document.oncontextmenu=function()
+        {
+            alert("Right click is disabled");
+            return false;
+        }
+    }
     return(
-        <div className=" container-fluid">
+        <div className=" container-fluid" onContextMenu={handleContextMenu}>
             <h2>Button Events</h2>
-            <img src="m1.jpg" width="150" height="150" onDoubleClick={handleDoubleClick}/>
+            <img src="m1.jpg" width="150" height="150" onDoubleClick={handleDoubleClick} />
            <p>double click to view large image</p>
         </div>
     )
